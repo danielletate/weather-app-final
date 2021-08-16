fdf225778be13ad05c5f16121450c4ab27af547f
danielletate <88792165+danielletate@users.noreply.github.com>
2021-08-16 11:31:17 -0600

    Added weather elements to JS file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-let apiKey = "7fc9dee93f614917a3bcd4e54c219855";
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.weather.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+}
+
+let apiKey = "1b2d6c00d1f7b2167a2d82e1ee956364";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
